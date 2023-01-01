b8ca4e9bb8a4a6a67f7da6e6c6b55d2eb0a2cc08
Oluwafisayomi-Madehin <faithfisayo88@gmail.com>
2023-01-01 13:27:02 +0100

    check and fix loop bug

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 userName = input("Enter your name: ")
 if len(userName) >= 10:
     print("Too much info - keep it shorter!")
@@ -5,7 +7,39 @@ else:
     print("Welcome " + userName)
 
 planets = ["Deuteron","Axton","Capricon","Galaticon"]
-print("Choose a planet between Deuteron, Axton, Capricon, Galaticon in the order 0,1,2,3")
+print("Choose a planet between in the order 0,1,2,3")
 
 userPlanet = input("Enter your planet: ")
 print("Welcome to " + planets[int(userPlanet)])
+
+randomNumber = random.randint(1000, 2500)
+inputValid = False
+
+def inputedNumber():
+    inputedNumber = int(input("Guess a number: "))
+    inputValidityChecker()
+    return inputedNumber
+
+def inputValidityChecker():
+    if inputedNumber() <= 1000:
+        inputValid = False
+        print("Too little!")
+
+    elif inputedNumber() > 1000 & inputedNumber() < 2500:
+        inputValid = True
+        if inputedNumber() <= randomNumber:
+            print("You lose!")
+
+        elif inputedNumber() < randomNumber:
+            print("You win!")
+
+        print(planets[int(userPlanet)] + " prepared " + str(randomNumber) + "!")
+
+    else:
+        print("Too large!")
+        inputValid = False
+
+
+while inputValid == False:
+    inputedNumber()
+    
